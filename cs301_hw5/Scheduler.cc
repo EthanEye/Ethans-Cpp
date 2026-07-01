e42da726cc869a2eb1994ae931cf743dfa2b0945
Ethan <ethaneye02@gmail.com>
2026-07-01 01:50:42 -0600

    implement file reading

--- cs301_hw5/Scheduler.cc
+++ cs301_hw5/Scheduler.cc
@@ -4,56 +4,94 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 
-class Process {
+class Process
+{
 public:
     int id;
     int arrival;
     int burst;
     int priority;
-    // etc.
 };
 
-class FCFS {
+class FCFS
+{
 public:
-    void schedule(std::vector<Process>& processes) {
+    void schedule(std::vector<Process> &processes)
+    {
         (void)processes;
     }
 };
 
-class SJF {
+class SJFP
+{
 public:
-    void schedule(std::vector<Process>& processes) {
+    void schedule(std::vector<Process> &processes)
+    {
         (void)processes;
     }
 };
 
-class PriorityScheduler {
+class PriorityScheduler
+{
 public:
-    void schedule(std::vector<Process>& processes) {
+    void schedule(std::vector<Process> &processes)
+    {
         (void)processes;
     }
 };
 
-int main(int argc, char* argv[]) {
+int run(std::vector<Process> processes)
+{
+    std::cout << " --- FCFS ---\n";
+    FCFS fcfs;
+    fcfs.schedule(processes);
 
-    if (argc < 2) {
-        std::cerr << "Usage: ./Scheduler <input_file>" << std::endl;
-        return 1;
-    }
+    std::cout << " --- SJFP ---\n";
+    SJFP sjfp;
+    sjfp.schedule(processes);
+
+    std::cout << " --- Priority ---\n";
+    PriorityScheduler prsh;
+    prsh.schedule(processes);
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+
     // ifstream opens file for reading
     std::ifstream file(argv[1]);
-    if (!file.is_open()) {
-        std::cerr << "Error: Could not open the file!" << std::endl;
+    if (!file.is_open() || argc < 2)
+    {
+        std::cerr << "Error: invalid file specified" << std::endl;
         return 1;
     }
-
     std::string line;
-    // read line by line until end of file
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl;
+    std::vector<Process> processes;
+
+    while (std::getline(file, line))
+    {
+        std::stringstream stream(line);
+        std::string token;
+
+        std::getline(stream, token, ',');
+        int id = std::stoi(token);
+
+        std::getline(stream, token, ',');
+        int arrival = std::stoi(token);
+
+        std::getline(stream, token, ',');
+        int burst = std::stoi(token);
+
+        std::getline(stream, token, ',');
+        int priority = std::stoi(token);
+
+        processes.push_back({id, arrival, burst, priority});
     }
 
     file.close();
-    return 0;
+    return run(processes);
 }
+
